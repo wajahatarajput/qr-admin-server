fb845b7ee1c9afbf509d721506e31af194eabfc6
wajahatarajput <wajahatarajput@gmail.com>
2024-04-01 00:12:42 +0500

    port change

--- server.js
+++ server.js
@@ -433,7 +433,7 @@ io.on('connection', socket => {
 });
 
 // Start server
-const PORT = process.env.PORT || 2000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
